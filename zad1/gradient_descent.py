cc0942b25a70dc00b5b9569ec262579d44e06bab
Miloszun <mizak.milosz@gmail.com>
2022-10-16 21:13:21 +0200

    finished transforming the algorithim for numpy arrays

--- zad1/gradient_descent.py
+++ zad1/gradient_descent.py
@@ -1,11 +1,13 @@
-from typing import List
+import math
+import copy
 from autograd import grad
+import numpy as np
 
 class LabFunc:
     def __init__(self, alpha, n):
         self.alpha = alpha
         self.n = n
-    def returnValue(self, x:List):
+    def returnValue(self, x):
         y = 0
         for i in range(1, self.n):
             y += pow(self.alpha, i - 1/self.n - 1) * pow(x[i - 1], 2)
@@ -22,28 +24,28 @@ class QuadraticFunc:
             y += self.a * pow(value, 2) + self.b * value + self.c
         return y
 
-def solver(f, x0: List, alpha):
+def gradientDescent(f, x0, lengthFactor, maxSteps):
     # krok początkowy - x0
     old_x = x0
-    grad_f = grad(f.returnValue)
-    alpha_k = alpha
+    gradFunc = grad(f.returnValue)
     # main loop
-    while True:
-        grad_k = grad_f(old_x)
-        new_x = [value - (alpha_k * grad_k) for value in old_x]
-        if grad_k <= alpha_k:
+    for i in range(maxSteps):
+        gradValue = gradFunc(old_x)
+        # new_x = [value - (lengthFactor * gradValue) for value in old_x]
+        new_x = []
+        for i, value in enumerate(old_x):
+            new_x.append(value - (lengthFactor * gradValue[i]))
+        if np.linalg.norm(gradValue) <= lengthFactor:
             return f.returnValue(new_x)
         elif f.returnValue(new_x) >= f.returnValue(old_x):
-            alpha_k / 2
+            lengthFactor = lengthFactor / 2
         else:
-            old_x = copy.deepcopy(new_x)
-
-def test(x):
-    return x
-
+            old_x = new_x
+    return f.returnValue(new_x)    
 
 if __name__ == "__main__":
-    # alpha should be very small
-    labFunc = LabFunc(1, 10)
+    # lengthFactor should be very small
+    labFunc = LabFunc(10, 10)
     testFunc = QuadraticFunc(1, 0, 0)
-    print(solver(testFunc, [50.0], 0.05))
+    # print(gradientDescent(testFunc, [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0], 0.05, 100))
+    print(gradientDescent(labFunc, np.array([0.0, 1.0, -1.0, 2.0, 2.1, 2.2, -2.1, -2.2, -3.0, 3.0]), 0.03, 5000))
