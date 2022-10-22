a2eac5cd34a76a0fc48d069401fcdd99cc21cedc
mmizak <01169211@pw.edu.pl>
2022-10-22 20:04:19 +0200

    Update zad1/gradient_descent.py

--- zad1/gradient_descent.py
+++ zad1/gradient_descent.py
@@ -2,7 +2,6 @@ import time
 import random
 import matplotlib.pyplot as plt
 from autograd import grad
-from matplotlib.scale import LogScale
 import numpy as np
 
 class LabFunc:
@@ -11,8 +10,8 @@ class LabFunc:
         self.n = n
     def returnValue(self, x):
         y = 0
-        for i in range(1, self.n + 1):
-            y += pow(self.alpha, i - 1/self.n - 1) * pow(x[i - 1], 2)
+        for i in range(self.n):
+            y += pow(self.alpha, i/self.n - 1) * pow(x[i], 2)
         return y
 
 class QuadraticFunc:
@@ -27,40 +26,46 @@ class QuadraticFunc:
         return y
 
 def gradientDescent(f, x0, lengthFactor, maxSteps):
+    localLengthFactor = lengthFactor
     old_x = x0
     gradFunc = grad(f.returnValue)
-    iterationTimes = []
+    old_value = f.returnValue(old_x)
+    iterationValues = [old_value]
     # main loop
     for i in range(maxSteps):
-        # start = time.time()
         gradValue = gradFunc(old_x)
-        new_x = old_x - gradValue * lengthFactor
-        #if np.linalg.norm(gradValue) <= lengthFactor:
-        #    return f.returnValue(new_x)
-        if f.returnValue(new_x) >= f.returnValue(old_x):
-            lengthFactor = lengthFactor / 2
+        new_x = old_x - (gradValue * localLengthFactor)
+        new_value = f.returnValue(new_x)
+        # print("old value= ", old_value, "\n",  "new value = ", new_value, "---",  i, "\n", "lengthFactor = ", localLengthFactor)
+        if np.linalg.norm(gradValue) <= lengthFactor * 0.1:
+            return f.returnValue(new_x), iterationValues 
+        if new_value >= old_value:
+            localLengthFactor = localLengthFactor * 0.1
         else:
             old_x = new_x
-        # end = time.time()
-        iterationTimes.append(f.returnValue(new_x))
-        print(f.returnValue(new_x))
-    return f.returnValue(new_x), iterationTimes    
+            old_value = f.returnValue(old_x)
+            localLengthFactor = lengthFactor
+        iterationValues.append(old_value)
+        # print(f.returnValue(new_x))
+    return f.returnValue(new_x), iterationValues    
 
 if __name__ == "__main__":
     # lengthFactor should be very small
-    maxSteps = 1000
-    lengthFactor = 0.001
-    iterations = [i for i in range(1, maxSteps + 1)]
+    maxSteps = 100
+    lengthFactor = 0.2
+    iterations = range(maxSteps + 1)
 
-    for i in [1, 10, 100]:
+    for i in [1, 10, 1000]:
         labFunc = LabFunc(i, 10)
-        minimum, iterationTimes = gradientDescent(labFunc, np.array([1.0, 2.0, 3.0, 4.0, 5.0, 1.0, 2.0, 3.0, 4.0, 5.0, 10.0]), float(lengthFactor), maxSteps)
-        plt.plot(iterations, iterationTimes, label=f"alpha = {i}, length factor = {lengthFactor}")
+        minimum, iterationTimes = gradientDescent(labFunc, np.array([1.0, 2.0, 3.0, 4.0, 5.0, 1.0, 2.0, 3.0, 4.0, 5.0]), float(lengthFactor), maxSteps)
+        #plt.plot(iterations[0:len(iterationTimes)], iterationTimes, label=f"alpha = {i}, length factor = {lengthFactor}")
+        plt.plot(range(len(iterationTimes)), iterationTimes, label=f"alpha = {i}, length factor = {lengthFactor}")
     
-    
-    # plt.yscale('log')
+    print("Len of iterationTimes = ",  len(iterationTimes))
+    plt.yscale('log')
     plt.xlabel("t")
     plt.ylabel("q(x)")
     plt.title("Values of q(x) over iterations")
     plt.legend(loc="upper right")
     plt.show()
+
