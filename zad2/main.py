816608a75184b026aeba83d831b85dbf1cf07205
Miloszun <mizak.milosz@gmail.com>
2022-11-02 18:21:52 +0100

    made values averaged

--- zad2/main.py
+++ zad2/main.py
@@ -1,29 +1,25 @@
 from cec2017.functions import f1, f9
 import numpy as np
 import random
-import copy
+import matplotlib.pyplot as plt
+from scipy.optimize import fmin
 
 
-def generatePopulation(f):
+def generatePopulation(f, dimensions, populationSize):
     population = []
-    for _ in range(100):
-        x = np.random.uniform(-100, 100, size=10)
+    for _ in range(populationSize):
+        x = np.random.uniform(-100, 100, size=dimensions)
         valueTup = (x, f(x))
         population.append(valueTup)
     return population
 
 def reprodution(population):
-    populationCopy = copy.deepcopy(population)
     newPopulation = []
     for _ in range(int(len(population) / 2)):
-        specimen1, specimen2 = random.choice(populationCopy), random.choice(populationCopy)
-        if specimen1[1] <= specimen2[1]:
-            newPopulation.append(specimen1)
-            newPopulation.append(copy.deepcopy(specimen1))
-        else:
-            newPopulation.append(specimen2)
-            newPopulation.append(copy.deepcopy(specimen2))
-
+        specimen1, specimen2 = random.choice(population), random.choice(population)
+        newSpecimen = specimen1 if specimen1[1] <= specimen2[1] else specimen2
+        newPopulation.append(newSpecimen)
+        newPopulation.append(newSpecimen)
     return newPopulation
 
 def mutation(population, mutationForce, f):
@@ -35,8 +31,8 @@ def mutation(population, mutationForce, f):
     return mutatedPopulation
 
 
-def evolution(startingPopulation, mutationForce, f, mutantsSize, populationSize, days):
-    population = startingPopulation
+def evolution(mutationForce, f, mutantsSize, populationSize, days, dimensions):
+    population = generatePopulation(f, dimensions, populationSize)
     bestSpecimens = []
     for _ in range(days):
         reproducedPopulation = reprodution(population)
@@ -46,16 +42,25 @@ def evolution(startingPopulation, mutationForce, f, mutantsSize, populationSize,
         sortedMutants = sorted(mutatedPopulation, key=lambda x : x[1])
 
         population = sorted(sortedPopulation[:(populationSize - mutantsSize)] + sortedMutants[:mutantsSize], key=lambda x : x[1])
-        bestSpecimens.append(population[0])
+        populationValues = [specimen[1] for specimen in population]
+        bestSpecimens.append(sum(populationValues)/len(populationValues))
     return bestSpecimens
 
 if __name__ == "__main__":
     populationSize = 100
-    mutantsSize = 80
-    population = generatePopulation(f1)
-    mutationForce = 3
-    days = 100
-    bestSpecimens = evolution(population, mutantsSize, f1, mutantsSize, populationSize, days)
-    for specimen in bestSpecimens:
-        print(specimen[1])
-    
+    mutantsSize = 10
+    mutationForce = 2
+    days = 500
+    dimensions = 10
+    bestSpecimens = evolution(mutantsSize, f1, mutantsSize, populationSize, days, dimensions)
+
+    # specimenValues = [specimen[1] for specimen in bestSpecimens]
+
+    for value in bestSpecimens:
+        print(value)
+
+    plt.yscale("log")
+    plt.plot(range(len(bestSpecimens)), bestSpecimens)
+    plt.show()
+
+    # fmin(f9,np.array([0,0,0,0,0]))
